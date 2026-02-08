ef282449699e26f35e599889daabb8d55fa7f542
DaLazyCat-1 <jyuan@livoniawarriors.org>
2026-02-07 19:15:23 -0500

    Tuning swerve pid

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,13 +24,13 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.5)
+        .withKP(100).withKI(0).withKD(0.0) //was kp = 100, kd = 0.5
         .withKS(0.1).withKV(2.49).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
+        .withKP(0.8).withKI(0).withKD(0) //was 0.1
         .withKS(0).withKV(0.124);
 
     // The closed-loop output type to use for the steer motors;
